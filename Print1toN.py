f8173d541e4812ae2f80d67c28866e84ae94b344
shivani <nshivani2002@gmail.com>
2023-07-21 07:43:16 +0530

    first commit

---
+++ Print1toN.py
@@ -0,0 +1,29 @@
+'''
+You are given an integer A, print 1 to A using using recursion.
+Note :- After printing all the numbers from 1 to A, print a new line.
+
+Input 1:
+A = 10
+
+Input 2:
+A = 5
+
+Output 1:
+1 2 3 4 5 6 7 8 9 10
+
+Output 2:
+1 2 3 4 5
+'''
+def print1toN(n):
+    if n==1:
+        print(1)
+    else:
+        print1toN(n-1)
+        print(n)
+    
+
+number = int(input())
+print1toN(number)
+print()
+
+
